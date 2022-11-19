3b85fff603f81dc051680f4a0cb1d0faa7fa8eeb
martirl11 <reyeslopezmarta2001@gmail.com>
2022-11-19 16:32:17 +0100

    AWE-4 he creado las vistas para la visualiazación de los productos

--- base/views.py
+++ base/views.py
@@ -19,10 +19,9 @@ def product_list(request, category_slug=None):
 def product_detail(request, id, slug):
     product = get_object_or_404(Product,
                                 id=id,
-                                slug=slug,
-                                available=True)
+                                slug=slug)
 
 
     return render(request,
-                  'shop/product/detail.html',
+                  'base/product/detail.html',
                   {'product': product})
